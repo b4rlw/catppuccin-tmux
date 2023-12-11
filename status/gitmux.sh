d942e80362f0e54a61cb9ab61f67beaa20209dd0
migueltc13 <migueltc13@gmail.com>
2023-12-11 18:12:23 +0000

    Create status/gitmux.sh

---
+++ status/gitmux.sh
@@ -0,0 +1,15 @@
+show_gitmux() {
+
+  local cwd=$(tmux display-message -p -F "#{pane_current_path}")
+  if ! command -v gitmux >/dev/null 2>&1; then
+    return
+  fi
+
+  local index=$1
+  local icon="$(get_tmux_option "@catppuccin_gitmux_icon" "")"
+  local color="$(get_tmux_option "@catppuccin_gitmux_color" "$thm_green")"
+  local text="$(get_tmux_option "@catppuccin_gitmux_text" "#(gitmux \"#{pane_current_path}\")")"
+
+  local module=$( build_status_module "$index" "$icon" "$color" "$text" )
+  echo "$module"
+}
