86abac89e6c8b6efbbf40ed60bdf2caf20b61142
migueltc13 <migueltc13@gmail.com>
2023-12-12 02:36:22 +0000

    Add gitmux config file

--- status/gitmux.sh
+++ status/gitmux.sh
@@ -8,7 +8,8 @@ show_gitmux() {
   local index=$1
   local icon="$(get_tmux_option "@catppuccin_gitmux_icon" "")"
   local color="$(get_tmux_option "@catppuccin_gitmux_color" "$thm_green")"
-  local text="$(get_tmux_option "@catppuccin_gitmux_text" "#(gitmux \"#{pane_current_path}\")")"
+  # local text="$(get_tmux_option "@catppuccin_gitmux_text" "#(gitmux \"#{pane_current_path}\")")"
+  local text="$(get_tmux_option "@catppuccin_gitmux_text" "#(gitmux -cfg $HOME/.config/.gitmux.conf \"#{pane_current_path}\")")"
 
   local module=$( build_status_module "$index" "$icon" "$color" "$text" )
   echo "$module"
